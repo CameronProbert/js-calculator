36fa55cece547488aabe8086bf3580a6d34e97a0
Cameron Probert <c.probert@hotmail.co.nz>
2018-02-18 12:03:33 +0900

    Added Display Methods, Split Input Methods

--- calculator.js
+++ calculator.js
@@ -1,132 +1,145 @@
 // Global Variables:
+
+/*Saved numbers, ready to process*/
 var entries = [];
+/*The number the calculator displays*/
 var temp = "";
+/*Should a '-' be added when saving temp to entries*/
 var negative = false;
+/*Entering a number should clear the temp array
+Used after pressing the equals button*/
 var toClear = true;
-var hasDecimal = false;
 
-//Button listener
-function input(input){
-  //recieve a number -> add to temp
-  if (input.match(/[0-9.]/)) {
-    if (temp !== "" && temp.split("").includes(".") && input === "."){
-      return;
-    }
-    if (toClear){
-      entries = [];
-      if (input === "."){
-        temp = "0.";
-      } else {
-        temp = input;
-      }
-    } else {
-      temp+=input;
-    }
-    displayTemp(temp);
-    toClear = false;
-  }
 
-  //recieve a non-equals symbol -> add temp number to entries[]
-  //then add symbol to entries and clear temp
-  else if (input.match(/[+-/*]/)){
+//Buttons input here (except clear and positive/negative)
+function input(input) {
+	// Check for input and call appropriate method
+	if (input.match(/[0-9.]/)) {
+		addNumeral(input);
+	} else if (input.match(/[+-/*]/)) {
+		addOperation(input);
+	} else if (input === "=") {
+		evaluate();
+	}
 
-    // if (entries.length > 0 && !entries[entries.length-1].match(/[0-9]/)){
-    //   entries.splice(entries.length, 1);
-    // }
+	//Redisplay calculator
+	console.log(entries);
+	displayHist();
+	displayTemp();
+}
 
-    if (negative){
-      entries.push('-' + temp);
-    } else {
-      entries.push(temp);
-    }
-    entries.push(input);
-    temp = "";
-    negative = false;
-    toClear = false;
-  }
-  // If '=' then evaluate the answer
-  else if (input === "=") {
-    if (negative){
-      entries.push('-' + temp);
-    } else {
-      entries.push(temp);
-    }
-    temp = "";
-    negative = false;
-    evaluate();
-  }
 
-  //clear last entry
-
-  //if equals sign then evaluate and display answer
-  {
-    //interpret negative symbol to change number to negative
-  }
-  console.log(entries);
-  displayHist();
+// Saves the temp into the entries array
+function saveValue() {
+	if (negative) {
+		entries.push('-' + temp);
+	} else {
+		entries.push(temp);
+	}
+	temp = "";
+	negative = false;
 }
 
 // Interprets and evaluates the entries array
-function evaluate(){
-  while (entries.length > 2){
-    var val1 = parseFloat(entries[0]);
-    var val2 = parseFloat(entries[2]);
-    var operation = entries[1];
+function evaluate() {
+	//First, save the current temp value
+	saveValue();
 
-    var result = 0;
-    if (operation === "+"){
-      result = val1 + val2;
-    }
-    if (operation === "-"){
-      result = val1 - val2;
-    }
-    if (operation === "/"){
-      result = val1 / val2;
-    }
-    if (operation === "*"){
-      result = val1 * val2;
-    }
+	// Loop through each operation
+	while (entries.length > 2) {
+		var val1 = parseFloat(entries[0]);
+		var val2 = parseFloat(entries[2]);
+		var operation = entries[1];
 
-    entries.splice(0, 2);
-    entries[0] = result;
-    temp = result;
-    toClear = true;
-  }
-  displayTemp(entries[0]);
-  entries = [entries[0]];
+		var result = 0;
+		if (operation === "+") {
+			result = val1 + val2;
+		}
+		if (operation === "-") {
+			result = val1 - val2;
+		}
+		if (operation === "/") {
+			result = val1 / val2;
+		}
+		if (operation === "*") {
+			result = val1 * val2;
+		}
+
+		entries.splice(0, 2);
+		entries[0] = result;
+		temp = result;
+		toClear = true;
+	}
+
+	// Clear the entries array (The answer is in temp)
+	entries = [];
 }
 
-function negate(){
-  negative = !negative;
-  displayTemp(temp);
+// Sets the 'negative' flag
+function negate() {
+	negative = !negative;
+	displayTemp(temp);
 }
 
 // Resets the calculator
-function clear(){
-  temp = "";
-  entries = [];
-  displayTemp("");
-  displayHist();
+function clear() {
+	temp = "";
+	entries = [];
+	displayTemp("");
+	displayHist();
+}
+
+// Adds a numeral or period to the temp display
+function addNumeral(input) {
+	// If there is already a period then don't add another
+	if (temp !== "" && temp.split("").includes(".") && input === ".") {
+		return;
+	}
+
+	if (toClear) {
+		if (input === ".") {
+			temp = "0.";
+		} else {
+			temp = input;
+		}
+	} else {
+		temp += input;
+	}
+	//displayTemp(temp);
+	toClear = false;
+}
+
+
+// Adds a mathematical operation to the entries array
+function addOperation(input) {
+	if (entries.length > 0 && entries[entries.length - 1].match(/[^0-9]/)) {
+		return;
+	}
+	saveValue();
+	entries.push(input);
+	temp = "";
+	negative = false;
+	toClear = false;
 }
 
 
 // vvvvvvvvvvvvvvvvvvvvvvvvvv Helper Functions vvvvvvvvvvvvvvvvvvvvvvvvvv
 
-// Displays the given text in the output box
-function displayTemp(text){
-  if (negative) {
-    document.getElementById("output").value = '-' + text;
-  } else {
-   document.getElementById("output").value = text;
-  }
-  console.log("setting text to: ", text);
+// Sets the value of the temp output box
+function displayTemp() {
+	if (negative) {
+		document.getElementById("output").value = '-' + temp;
+	} else {
+		document.getElementById("output").value = temp;
+	}
+	console.log("setting text to: ", temp);
 }
 
-// Displays the given text in the output box
-function displayHist(){
-  var text = "";
-  for (var i = 0; i < entries.length; i++){
-    text+=entries[i] + " ";
-  }
-  document.getElementById("history").innerHTML = text;
+// Displays the given text in the history box
+function displayHist() {
+	var text = "";
+	for (var i = 0; i < entries.length; i++) {
+		text += entries[i] + " ";
+	}
+	document.getElementById("history").innerHTML = text;
 }
